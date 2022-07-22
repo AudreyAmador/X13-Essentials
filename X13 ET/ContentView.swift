ccc08e43fdd1ce29046c25612f99063b7c16fdc5
Audrey Amador <aamador@baccredomatic.cr>
2022-07-22 10:23:46 -0600

    commit de prueba

--- X13 ET/ContentView.swift
+++ X13 ET/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, world! hice un cambio")
             .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewInterfaceOrientation(.portraitUpsideDown)
+            ContentView()
+                .previewInterfaceOrientation(.portraitUpsideDown)
+        }
     }
 }
