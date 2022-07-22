24369f7537ab9307ac342d2be941995f14a062a6
Audrey Amador <aamador@baccredomatic.cr>
2022-07-22 09:09:48 -0600

    Initial Commit

---
+++ X13 ET/X13_ETApp.swift
@@ -0,0 +1,17 @@
+//
+//  X13_ETApp.swift
+//  X13 ET
+//
+//  Created by Audrey Amador on 22/7/22.
+//
+
+import SwiftUI
+
+@main
+struct X13_ETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ X13 ETUITests/X13_ETUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  X13_ETUITestsLaunchTests.swift
+//  X13 ETUITests
+//
+//  Created by Audrey Amador on 22/7/22.
+//
+
+import XCTest
+
+class X13_ETUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
